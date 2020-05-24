ecd676dfa61ba134bacd2b981a647789be79d0ab
StechAnurag <anuragtech24.7@gmail.com>
2020-05-24 16:32:26 +0530

    object to map

--- src/Views/Home/ItemDetails/ItemDetails.js
+++ src/Views/Home/ItemDetails/ItemDetails.js
@@ -23,116 +23,107 @@ class ItemDetails extends Component {
       return <div className={DetailClass.ErrMessage}>ERROR: {documentStateData.error}</div>;
     }
 
-    const itemDetails = documentStateData.reMagzineData.magzine;
     return (
       <div className="fadeInEffect">
         <div className={DetailClass.itemDetailWidth}>
           <div className="row clr-margin">
-            <div
-              className={`col-xl-5 col-lg-6 col-md-6 col-sm-12 col-12 my-5 ${DetailClass.itemMargin}`}>
-              <div className={DetailClass.itemCard}>
-                <div className={DetailClass.topSection}>
-                  <img
-                    id={DetailClass.imageContainer}
-                    src={`${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${
-                      itemDetails && itemDetails.coverImages[0]
-                    }`}
-                    alt=""
-                  />
-                  <div className={DetailClass.selectImage}>
-                    <img
-                      onClick={() =>
-                        this.imageChangeHandler(
-                          `${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${
-                            itemDetails && itemDetails.coverImages[0]
-                          }`
-                        )
-                      }
-                      src={`${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${
-                        itemDetails && itemDetails.coverImages[0]
-                      }`}
-                      alt=""
-                    />
-                    <img
-                      onClick={() =>
-                        this.imageChangeHandler(
-                          `${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${
-                            itemDetails && itemDetails.coverImages[1]
-                          }`
-                        )
-                      }
-                      src={`${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${
-                        itemDetails && itemDetails.coverImages[1]
-                      }`}
-                      alt=""
-                    />
-                  </div>
-                  <div className={DetailClass.price}>{`₹${itemDetails && itemDetails.price}`}</div>
-                </div>
+            {documentStateData.reMagzineData.map((data) => {
+              return (
+                <React.Fragment key={data._id}>
+                  <div
+                    className={`col-xl-5 col-lg-6 col-md-6 col-sm-12 col-12 my-5 ${DetailClass.itemMargin}`}>
+                    <div className={DetailClass.itemCard}>
+                      <div className={DetailClass.topSection}>
+                        <img
+                          id={DetailClass.imageContainer}
+                          src={`${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${data.coverImages[0]}`}
+                          alt=""
+                        />
+                        <div className={DetailClass.selectImage}>
+                          <img
+                            onClick={() =>
+                              this.imageChangeHandler(
+                                `${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${data.coverImages[0]}`
+                              )
+                            }
+                            src={`${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${data.coverImages[0]}`}
+                            alt=""
+                          />
+                          <img
+                            onClick={() =>
+                              this.imageChangeHandler(
+                                `${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${data.coverImages[1]}`
+                              )
+                            }
+                            src={`${process.env.REACT_APP_BASE_URL}/magzine/get-cover/${data.coverImages[1]}`}
+                            alt=""
+                          />
+                        </div>
+                        <div className={DetailClass.price}>{`₹${data.price}`}</div>
+                      </div>
 
-                <div className={DetailClass.productInfo}>
-                  <div className={DetailClass.name}>{itemDetails && itemDetails.name}</div>
-                  <Link to="#" className={`${DetailClass.btn} ${DetailClass.btnAnimated}`}>
-                    Buy Now
-                  </Link>
-                </div>
-              </div>
-            </div>
-            <div
-              className={`col-xl-7 col-lg-6 col-md-6 col-sm-12 col-12 my-5 ${DetailClass.itemMargin}`}>
-              <div className={DetailClass.description}>
-                <h1>{itemDetails && itemDetails.name}</h1>
-                <div>
-                  <span className={DetailClass.badge}>{itemDetails && itemDetails.tags}</span>
-                  <span className={DetailClass.language}>
-                    {itemDetails && itemDetails.language}
-                  </span>
-                </div>
-                <div className={DetailClass.specialPrices}>
-                  <span>Special price</span>
-                </div>
-                <div className={DetailClass.prices}>
-                  <div className={DetailClass.pricesTag}>{`₹${
-                    itemDetails && itemDetails.price
-                  }`}</div>
-                </div>
-                <div>
-                  <p>{itemDetails && itemDetails.description}</p>
-                </div>
-                <div className={DetailClass.descPoint}>
-                  <div className={DetailClass.pointsLine}>
-                    <img
-                      src="https://rukminim1.flixcart.com/www/36/36/promos/06/09/2016/49f16fff-0a9d-48bf-a6e6-5980c9852f11.png?q=90"
-                      alt=""
-                      width="18"
-                      height="18"
-                      className={DetailClass.pointImage}
-                    />
-                    <li>{itemDetails && itemDetails.frequency}</li>
+                      <div className={DetailClass.productInfo}>
+                        <div className={DetailClass.name}>{data.name}</div>
+                        <Link to="#" className={`${DetailClass.btn} ${DetailClass.btnAnimated}`}>
+                          Buy Now
+                        </Link>
+                      </div>
+                    </div>
                   </div>
-                  <div className={DetailClass.pointsLine}>
-                    <img
-                      src="https://img.icons8.com/cute-clipart/64/000000/time-span.png"
-                      alt=""
-                      width="18"
-                      height="18"
-                      className={DetailClass.pointImage}
-                    />
-                    <li>{itemDetails && itemDetails.month}</li>
+                  <div
+                    className={`col-xl-7 col-lg-6 col-md-6 col-sm-12 col-12 my-5 ${DetailClass.itemMargin}`}>
+                    <div className={DetailClass.description}>
+                      <h1>{data.name}</h1>
+                      <div>
+                        <span className={DetailClass.badge}>{data.tags}</span>
+                        <span className={DetailClass.language}>{data.language}</span>
+                      </div>
+                      <div className={DetailClass.specialPrices}>
+                        <span>Special price</span>
+                      </div>
+                      <div className={DetailClass.prices}>
+                        <div className={DetailClass.pricesTag}>{`₹${data.price}`}</div>
+                      </div>
+                      <div>
+                        <p>{data.description}</p>
+                      </div>
+                      <div className={DetailClass.descPoint}>
+                        <div className={DetailClass.pointsLine}>
+                          <img
+                            src="https://rukminim1.flixcart.com/www/36/36/promos/06/09/2016/49f16fff-0a9d-48bf-a6e6-5980c9852f11.png?q=90"
+                            alt=""
+                            width="18"
+                            height="18"
+                            className={DetailClass.pointImage}
+                          />
+                          <li>{data.frequency}</li>
+                        </div>
+                        <div className={DetailClass.pointsLine}>
+                          <img
+                            src="https://img.icons8.com/cute-clipart/64/000000/time-span.png"
+                            alt=""
+                            width="18"
+                            height="18"
+                            className={DetailClass.pointImage}
+                          />
+                          <li>{data.month}</li>
+                        </div>
+                        <div className={DetailClass.pointsLine}>
+                          <img
+                            src="https://img.icons8.com/color/48/000000/shuttlecraft-type-9.png"
+                            alt=""
+                            width="18"
+                            height="18"
+                            className={DetailClass.pointImage}
+                          />
+                          <li>{data.type}</li>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-                  <div className={DetailClass.pointsLine}>
-                    <img
-                      src="https://img.icons8.com/color/48/000000/shuttlecraft-type-9.png"
-                      alt=""
-                      width="18"
-                      height="18"
-                      className={DetailClass.pointImage}
-                    />
-                    <li>{itemDetails && itemDetails.type}</li>
-                  </div>
-                </div>
-              </div>
-            </div>
+                </React.Fragment>
+              );
+            })}
           </div>
         </div>
       </div>
