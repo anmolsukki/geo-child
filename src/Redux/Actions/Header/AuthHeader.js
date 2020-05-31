cd5bf26525a245fcab90a651ebbaa8fa6226b469
anmolsukki <anmolsukki94@gmail.com>
2020-06-01 00:47:30 +0530

    insecure policy

--- src/Redux/Actions/Header/AuthHeader.js
+++ src/Redux/Actions/Header/AuthHeader.js
@@ -2,6 +2,7 @@ export const getHeaders = (isAuth = false) => {
   if (isAuth) {
     return {
       'Content-Type': 'application/json',
+      'Content-Security-Policy': 'block-all-mixed-content',
       Authorization: `Bearer ${localStorage.getItem('token')}`,
     };
   }
